0c4781575e374d021b01a4accc8fbbaa4cfe4409
anushik <hovhannisyan.an98@gmail.com>
2020-03-12 11:01:22 +0400

    updating client directory

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,16 +1,46 @@
-
 #include "client.hpp"
+#include "commands/commands.hpp"
 
 #include <iostream>
+#include <cassert>
 
-int hello()
+Client::Client() noexcept
 {
-	std::cout << "Hi from client \n";
-	return 0;
+    m_ipv4 = new SocketAddress("127.0.0.1:8000");
+    m_clientSocket = new StreamSocket(*m_ipv4);
+    
+    std::string s("Give me some information");
+    m_outputBuffer = s.c_str();
+    m_clientSocket->sendBytes(m_outputBuffer, sizeof(s));
+
+//    int k = m_clientSocket->receiveBytes(m_inputBuffer,50);
+//   std::cout << "Buffer " << k << " " << (std::string)m_inputBuffer << std::endl;     
+  
 }
 
-int main()
+Client::Client(const Client& other) noexcept
 {
-	hello();
-	return 0;
+    m_ipv4 = other.m_ipv4;
+    m_clientSocket = other.m_clientSocket;
+    m_inputBuffer = other.m_inputBuffer;
+    m_outputBuffer = other.m_outputBuffer;
+}
+
+Client& Client::operator=(const Client&& other) noexcept
+{
+    delete m_inputBuffer;
+    delete m_outputBuffer;
+    m_ipv4 = std::move(other.m_ipv4);
+    m_clientSocket = std::move(other.m_clientSocket);
+    m_inputBuffer = std::move(other.m_inputBuffer);
+    m_outputBuffer = std::move(other.m_outputBuffer);
+    return *this;
+}
+
+Client::~Client() noexcept
+{    
+    delete m_inputBuffer;
+    delete m_outputBuffer;
+    assert(nullptr != m_clientSocket);
+    m_clientSocket->close();
 }
--- src/client/client.hpp
+++ src/client/client.hpp
@@ -1,2 +1,26 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
 
-int hello();
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/SocketAddress.h>
+
+using SocketAddress = Poco::Net::SocketAddress;
+using StreamSocket = Poco::Net::StreamSocket;
+
+class Client 
+{
+private:
+    SocketAddress* m_ipv4;
+    StreamSocket* m_clientSocket;
+    const char* m_outputBuffer;
+    char* m_inputBuffer;
+
+public:
+    Client() noexcept;
+    Client(const Client& other) noexcept;
+    Client& operator=(const Client&& other) noexcept;
+    ~Client() noexcept;
+
+};
+
+#endif // CLIENT_HPP
