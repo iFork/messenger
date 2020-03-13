5bb70882df3db54d004d3b98c6b7db7cc98ba18d
Vardan Vardanyan <vardanyan.vardan477@gmail.com>
2020-03-13 13:20:08 +0400

    added parser

---
+++ parser/main.cpp
@@ -0,0 +1,29 @@
+#include "json_parser.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace Poco::JSON;
+using namespace Poco::Dynamic;
+
+int main(int argc, char** argv) {
+	std::string json_file;
+	std::string file;
+	// insted of writing Poco::JSON::Parser used Parser
+	//
+	Parser parse_from;
+	File_writer w("./w_file.json");
+	file_writer(w, file, json_file);
+	// Parse the JSON and get the Results
+	//insted of writing Poco::Dynamic::Var used Var
+	//
+	Var parsed_json = parse_from.parse(json_file);
+	Var parsed_json_result = parse_from.result();
+	// Get the JSON Object
+	// insted of writing Poco::JSON::Object::Ptr used Object::Ptr
+	//
+	Object::Ptr json_object_value = parsed_json_result.extract<Object::Ptr>();
+	// Get the values for the member variables and print
+	//
+	print_after_pars(json_file, json_object_value);
+	return 0;
+}
